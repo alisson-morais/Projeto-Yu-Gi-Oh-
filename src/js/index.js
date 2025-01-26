f76b779e352801a0ad93337dbb72a5d753f40a87
alisson-morais <alissonsilvamoraisdossantos@gmail.com>
2025-01-26 12:35:03 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,34 @@
+
+const btnavancar = document.getElementById("btn-avancar");
+const cartoes = document.querySelectorAll(".cartao");
+let cartaoAtual = 0;
+const btnVoltar = document.getElementById("btn-voltar");
+
+btnavancar.addEventListener("click",function(){  
+  if(cartaoAtual === cartoes.length - 1) return;
+
+  esconderCartaoSelecionado();
+
+  cartaoAtual++;
+  mostrarCartao();
+  
+});
+
+btnVoltar.addEventListener("click",function(){  
+  if(cartaoAtual === 0) return;
+  
+esconderCartaoSelecionado();
+   
+  cartaoAtual--;
+ mostrarCartao();
+  
+});
+
+function mostrarCartao() {
+  cartoes[cartaoAtual].classList.add("selecionado");
+}
+
+function esconderCartaoSelecionado() {
+  const cartaoSelecionado = document.querySelector(".selecionado");
+  cartaoSelecionado.classList.remove("selecionado");
+}
